37f71f8f7b7a6f70bf7b76b0cce7a8b1a513ee7b
ZaneWallace20 <138349317+ZaneWallace20@users.noreply.github.com>
2026-07-25 22:42:16 -0500

    Start of rotatoin at 0,0 idk needs to rotate a lot use the new script the player roatation one

---
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField] private List<Transform> transformsToLookAtZeroZero;
+
+    private void Start()
+    {
+        if (transformsToLookAtZeroZero == null)
+        {
+            Debug.LogError("Transforms to look at zero zero is not assigned.");
+        }
+
+        for (int i = 0; i < transformsToLookAtZeroZero.Count; i++)
+        {
+            if (transformsToLookAtZeroZero[i] == null)
+            {
+                Debug.LogError($"Transform at index {i} is not assigned.");
+            }
+            transformsToLookAtZeroZero[i].rotation = Quaternion.LookRotation(Vector3.zero - transformsToLookAtZeroZero[i].position, Vector3.up); 
+        }
+    }
+}
